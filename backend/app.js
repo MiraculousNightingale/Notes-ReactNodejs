dd878c1428b3d903fb0be16b7953f1b720031313
Nightingale <esteticwence@gmail.com>
2023-10-29 20:11:00 +0200

    Add Sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,30 @@
+require("dotenv").config();
+const ENV = process.env;
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: ENV.DB_DIALECT,
+  username: ENV.DB_USERNAME,
+  password: ENV.DB_PASSWORD,
+  host: ENV.DB_HOST,
+  port: ENV.DB_PORT,
+  database: ENV.DB_NAME,
+});
+
+sequelize
+  .authenticate({})
+  .then((_) => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database:", error);
+  });
 
 const noteRoutes = require("./api/routes/notes");
 
